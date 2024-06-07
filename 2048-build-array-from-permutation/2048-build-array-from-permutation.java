1abda5852ce9063a1350d04d7f259477700c1c56
Poojasadgir <147875871+Poojasadgir@users.noreply.github.com>
2024-06-07 18:33:31 +0530

    Time: 1 ms (98.32%), Space: 45.1 MB (50.64%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,9 +1,6 @@
 class Solution {
     public int[] buildArray(int[] nums) {
       int n =nums.length;
-    //   if(n==0){
-    //     return 0;
-    //   }
       int ans[]=new int [n];  
       for(int i=0;i<nums.length;i++){
         ans[i]=nums[nums[i]];
