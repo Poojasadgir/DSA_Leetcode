b94123331197644eb2988b0aea9d82c9e3965d6d
Pooja Sadgir <147875871+Poojasadgir@users.noreply.github.com>
2024-09-07 22:04:40 +0530

    Time: 14 ms (5.86%), Space: 45.3 MB (8.32%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,63 @@
+class Solution {
+    private int k;
+    private List<String>res;
+    class TrieNode{
+        TrieNode[]children;
+        boolean isWord;
+        public TrieNode()
+        {
+            children=new TrieNode[26];
+            isWord=false;
+        }
+    }
+    public List<String> topKFrequent(String[] words, int k) {
+        this.k=k;
+        int n=words.length;
+        res=new ArrayList<>();
+        TrieNode[]bucket=new TrieNode[n+1];
+        Map<String,Integer>cnt=new HashMap<>();
+        for(String word:words){
+            cnt.put(word,cnt.getOrDefault(word,0)+1);
+        }  
+        for(var entry: cnt.entrySet()){
+            if(bucket[entry.getValue()]==null){
+                bucket[entry.getValue()]=new TrieNode();
+            }
+            addWord(bucket[entry.getValue()],entry.getKey());
+        }
+
+        for(int i=n;i>=0;i--){
+            if(bucket[i]!=null){
+                getWords(bucket[i],"");
+            }
+            if(k==0){
+                break;
+            }
+        }
+            return res;
+        }
+        private void addWord(TrieNode root,String word){
+            TrieNode cur=root;
+            for(char c:word.toCharArray()){
+                if(cur.children[c-'a']==null){
+                    cur.children[c-'a']=new TrieNode();
+                }
+                cur=cur.children[c-'a'];
+            }
+            cur.isWord=true;
+        }
+         private void getWords(TrieNode root,String prefix){
+            if(k<=0){
+                return;
+            }
+            if(root.isWord==true){
+                k--;
+                res.add(prefix);
+            }
+            for(int i=0;i<26;i++){
+                if(root.children[i]!=null){
+                    getWords(root.children[i],prefix+(char)(i+'a'));
+                }
+            }
+        }
+}
