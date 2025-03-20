fcf3ada52cc2564490e0092e9f0356238083ee50
Pooja Sadgir <147875871+Poojasadgir@users.noreply.github.com>
2025-03-20 12:30:37 +0530

    Time: 10 ms (9.82%), Space: 46.7 MB (67.21%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] countBits(int n) {
+      int ans[]=new int[n+1];
+      ans[0]=0;
+      for(int i=1;i<=n;i++){
+        String binary=Integer.toBinaryString(i);
+        int count=onesCount(binary);
+        ans[i]=count;
+      }  
+      return ans;
+    }
+    static int onesCount(String binary){
+        int count=0;
+        for(int i=0;i<binary.length();i++){
+            if(binary.charAt(i)=='1'){
+                count++;
+            }
+        }
+        return count;
+    }
+}
