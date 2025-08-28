a281c774f0263615f84b2697fbbaea2b08f017f2
Pooja Sadgir <147875871+Poojasadgir@users.noreply.github.com>
2025-08-28 18:52:43 +0530

    Time: 1 ms (99.84%), Space: 46.2 MB (60.66%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result=0;
-        for(int i=0;i<nums.length;i++){
-            result^=nums[i];
+        int n=nums.length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=ans^nums[i];
         }
-       return result; 
+        return ans;
     }
 }
