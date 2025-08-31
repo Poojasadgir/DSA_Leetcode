aa29002bc676e353e1a25ad1b5842e4ad994d9cf
Pooja Sadgir <147875871+Poojasadgir@users.noreply.github.com>
2025-08-31 11:38:18 +0530

    Time: 5 ms (24.51%), Space: 46.1 MB (7.23%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,10 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n_xor=nums.length;
-        for(int i=0;i<nums.length;i++){
-            n_xor=n_xor^i;
-            n_xor=n_xor^nums[i];
+        int start=0;
+        Arrays.sort(nums);
+        while( start<nums.length && start==nums[start]){
+            start++;
         }
-        return n_xor;
+        return start;
     }
 }
