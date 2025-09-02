084572c91d7828e001e2f05b9ec2eab377d72e34
Pooja Sadgir <147875871+Poojasadgir@users.noreply.github.com>
2025-09-02 10:41:47 +0530

    Time: 0 ms (100%), Space: 42.5 MB (14.59%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    private void reverseRow(int[] row) {
+        int left = 0, right = row.length - 1;
+        while (left < right) {
+            int temp = row[left];
+            row[left] = row[right];
+            row[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<m;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++){
+            reverseRow(matrix[i]);
+        }
+    }
+}
