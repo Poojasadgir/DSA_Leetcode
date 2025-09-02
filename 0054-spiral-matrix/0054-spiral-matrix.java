3d3c2e4fe58647e4c4b76426e425d95d4df8d5a9
Pooja Sadgir <147875871+Poojasadgir@users.noreply.github.com>
2025-09-02 12:31:08 +0530

    Time: 0 ms (100%), Space: 42 MB (8.68%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,36 +1,33 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-       List<Integer> list = new ArrayList<>();
-        int startRow = 0;
-        int startCol = 0;
-        int endRow = matrix.length - 1;
-        int endCol = matrix[0].length - 1;
-
-        while (startRow <= endRow && startCol <= endCol) {
-            for (int j = startCol; j <= endCol; j++) {
-                list.add(matrix[startRow][j]);
+        int startRow=0;
+        int startCol=0;
+        List<Integer>list=new ArrayList<>();
+        int endRow=matrix.length-1;
+        int endCol=matrix[0].length-1;
+        while(startRow<=endRow && startCol<=endCol){
+            for(int i=startCol;i<=endCol;i++){
+                list.add(matrix[startRow][i]);
             }
-            startRow++; 
-
+            startRow++;
             
-            for (int i = startRow; i <= endRow; i++) {
+            for(int i=startRow;i<=endRow;i++){
                 list.add(matrix[i][endCol]);
             }
-            endCol--; 
-           
-            if (startRow <= endRow) {  
-                for (int j = endCol; j >= startCol; j--) {
-                    list.add(matrix[endRow][j]);
+            endCol--;
+
+            if(startRow<=endRow){
+                for(int j=endCol;j>=startCol;j--){
+                 list.add(matrix[endRow][j]);
                 }
                 endRow--;
             }
 
-           
-            if (startCol <= endCol) {  
-                for (int i = endRow; i >= startRow; i--) {
-                    list.add(matrix[i][startCol]);
-                }
-                startCol++; 
+            if(startCol<=endCol){
+               for(int i=endRow;i>=startRow;i--){
+                 list.add(matrix[i][startCol]);
+               }
+               startCol++;
             }
         }
         return list;
