a5387ab155ad917202f09b5756594c3b3bd92432
Pooja Sadgir <147875871+Poojasadgir@users.noreply.github.com>
2025-09-02 19:33:09 +0530

    Time: 2 ms (4%), Space: 46.3 MB (8.01%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left=0,right=nums.length;
+        while(left<right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return -1;
+    }
+}
