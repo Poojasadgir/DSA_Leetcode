494dba66fed3d7a625ac33b4921869cc337ccfcb
Pooja Sadgir <147875871+Poojasadgir@users.noreply.github.com>
2025-09-07 18:59:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a Doubly Linked List/reverse-a-doubly-linked-list.java
@@ -0,0 +1,34 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+*/
+class Solution {
+    public Node reverse(Node head) {
+        // code here
+        if (head == null || head.next == null) return head;
+
+        Node curr = head;
+        Node prev = null;
+
+        while (curr != null) {
+            prev = curr.prev;
+            curr.prev = curr.next;
+            curr.next = prev;
+            curr = curr.prev;
+        }
+        if (prev != null) {
+            head = prev.prev;
+        }
+
+        return head;
+    }
+}
