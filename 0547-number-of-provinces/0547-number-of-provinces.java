e3990c4ee0109b3c280b0c4f67722fca250465eb
Pooja Sadgir <147875871+Poojasadgir@users.noreply.github.com>
2025-09-21 15:51:10 +0530

    Time: 3 ms (34.29%), Space: 46.9 MB (85.49%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,36 +1,37 @@
 class Solution {
-    public void  dfs(int node,List<Integer>graph[],boolean vis[]){
-        vis[node]=true;
-        for (int neighbor : graph[node]) {
+    public void dfs(int node, ArrayList<ArrayList<Integer>> graph, boolean[] vis) {
+        vis[node] = true;
+        for (int i=0;i<graph.get(node).size();i++) {
+            int neighbor=graph.get(node).get(i);
             if (!vis[neighbor]) {
                 dfs(neighbor, graph, vis);
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
-        int V=isConnected.length;
-        List<Integer> adjLs[] = new ArrayList[V]; // Declare array of lists
+        int V = isConnected.length;
         
-        // Initialize each index with an empty ArrayList
+        ArrayList<ArrayList<Integer>> adjLs = new ArrayList<>();
         for (int i = 0; i < V; i++) {
-            adjLs[i] = new ArrayList<>();
+            adjLs.add(new ArrayList<>());
         }
 
-        // Convert adjacency matrix to adjacency list
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
                 if (isConnected[i][j] == 1 && i != j) {
-                    adjLs[i].add(j);
-                    adjLs[j].add(i);
+                    adjLs.get(i).add(j);
+                    adjLs.get(j).add(i);
                 }
             }
         }
-        boolean vis[]=new boolean[V];
-        int cnt=0;
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
+
+        boolean[] vis = new boolean[V];
+        int cnt = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
                 cnt++;
-                dfs(i,adjLs,vis);
+                dfs(i, adjLs, vis);
             }
         }
         return cnt;
