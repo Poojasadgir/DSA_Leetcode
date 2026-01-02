9a29712ab83f00fc40de4a9a3db87d5e6920bc82
Pooja Sadgir <147875871+Poojasadgir@users.noreply.github.com>
2026-01-02 10:38:19 +0530

    Time: 2 ms (84.32%), Space: 47.9 MB (60.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,20 +1,12 @@
 class Solution {
-    static HashMap<Integer,Boolean>memo;
-    public boolean helper(int[]nums,int i){
-        if(i==nums.length-1) return true;
-        if(i>=nums.length || nums[i]==0) return false;
-        if(memo.containsKey(i)) return false;
-
-        boolean res=false;
-        for(int j=1;j<=nums[i];j++){
-            res=helper(nums,i+j);
-            if(res) return true;
-        }
-        memo.put(i,false);
-        return false;
-    }
     public boolean canJump(int[] nums) {
-        memo=new HashMap<>();
-        return helper(nums,0);
+        int n=nums.length;
+        int maxLen=0;
+        for(int i=0;i<n;i++){
+            if(i>maxLen)return false;
+
+            maxLen=Math.max(maxLen,i+nums[i]);
+        }
+        return true;
     }
 }
